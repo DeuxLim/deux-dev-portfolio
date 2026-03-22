19ffcae6ad63a30991675ecd6a25f30b0f2b16b2
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-22 19:32:03 +0800

    main layout bg dynamic

--- client/src/layouts/PortfolioLayout.tsx
+++ client/src/layouts/PortfolioLayout.tsx
@@ -1,8 +1,12 @@
+import useTheme from "@/context/Theme/useTheme";
 import { Outlet } from "react-router";
 
 export default function PortfolioLayout() {
+	const { isDarkMode } = useTheme();
 	return (
-		<div className="h-screen bg-white bg-cover bg-center p-8">
+		<div
+			className={`${isDarkMode ? "bg-black" : "bg-white"} h-screen bg-cover bg-center p-8`}
+		>
 			{/* Main Template */}
 			<div className="max-w-4xl mx-auto px-4 py-8">
 				<Outlet />
