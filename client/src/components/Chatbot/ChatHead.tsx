8b2ab84984955af9e27526fd68da7ce7f6d3baca
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-25 17:03:47 +0800

    updated floating chathead

--- client/src/components/Chatbot/ChatHead.tsx
+++ client/src/components/Chatbot/ChatHead.tsx
@@ -7,36 +7,46 @@ export default function ChatHead() {
 
 	return (
 		<div className="rounded-full hover:shadow-lg group fixed bottom-10 right-[4%] flex items-center justify-center gap-2">
-			<div className="relative size-20">
-				<div className="size-20 rounded-full overflow-hidden">
+			<div className="relative size-16">
+				{/* Avatar */}
+				<div className="size-16 rounded-full overflow-hidden">
 					<img src={me} alt="" />
 				</div>
 
 				{/* Active Status */}
-				<div className="bg-green-400 rounded-full size-5 absolute bottom-0 right-0"></div>
+				<div className="bg-green-400 rounded-full size-4 absolute bottom-0 right-0"></div>
 
 				{/* Close Button */}
 				<div
-					className={`group-hover:flex top-0 right-0  size-6 absolute hidden justify-center items-center ${isDarkMode ? "bg-white text-black" : "bg-gray-900 text-white"} rounded-full`}
+					className={`group-hover:flex top-0 right-0 size-5 absolute hidden justify-center items-center ${
+						isDarkMode
+							? "bg-white text-black"
+							: "bg-gray-900 text-white"
+					} rounded-full`}
 				>
-					<IoCloseSharp />
+					<IoCloseSharp size={14} />
 				</div>
 
 				{/* Chat Preview */}
 				<div
-					className={`group-hover:flex hidden rounded-lg shadow-lg absolute -top-4 -left-44 w-40  ${!isDarkMode ? "text-white bg-black" : "text-black bg-white"}`}
+					className={`group-hover:flex hidden rounded-lg shadow-[0_2px_4px_-2px_rgba(0,0,0,0.15)] absolute -top-3 -left-40 w-36 ${
+						!isDarkMode
+							? "text-white bg-black"
+							: "text-black bg-white"
+					}`}
 				>
-					<div className="flex flex-col p-4">
+					<div className="flex flex-col p-3 text-sm">
 						<div className="font-semibold">Deux Lim</div>
 						<div className="font-light">Hi, Let's chat!</div>
 					</div>
 
+					{/* Arrow */}
 					<div className="absolute top-[60%] left-full">
 						<div
 							className={`w-0 h-0 
-									border-t-8 border-t-transparent
-									border-b-8 border-b-transparent
-									border-l-12 ${isDarkMode ? "border-l-white" : "border-l-black"} `}
+								border-t-6 border-t-transparent
+								border-b-6 border-b-transparent
+								border-l-10 ${isDarkMode ? "border-l-white" : "border-l-black"}`}
 						></div>
 					</div>
 				</div>
