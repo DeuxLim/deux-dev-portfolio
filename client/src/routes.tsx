8eed74b7027c5401ce57e9ba8699e697817df203
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-26 17:50:21 +0800

    setup query client provider

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -2,14 +2,19 @@ import { createBrowserRouter } from "react-router";
 import PortfolioLayout from "./layouts/PortfolioLayout";
 import Home from "@/pages/Home";
 import ChatProvider from "./context/Chat/ChatProvider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const routes = createBrowserRouter([
 	{
 		path: "/",
 		element: (
-			<ChatProvider>
-				<PortfolioLayout />
-			</ChatProvider>
+			<QueryClientProvider client={queryClient}>
+				<ChatProvider>
+					<PortfolioLayout />
+				</ChatProvider>
+			</QueryClientProvider>
 		),
 		children: [
 			{
