684faf59a32fa8064d4d18251396e0d41df5dab1
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-26 18:00:55 +0800

    chat initial functionality

--- client/src/components/Chatbot/ChatBox.tsx
+++ client/src/components/Chatbot/ChatBox.tsx
@@ -1,9 +1,35 @@
 import useChat from "@/context/Chat/useChat";
 import { FaMinus, FaThumbsUp } from "react-icons/fa6";
 import ChatIntro from "@/components/Chatbot/ChatIntro";
+import { useMutation } from "@tanstack/react-query";
+import { api } from "@/lib/axios.client";
+import { useEffect, useState } from "react";
 
 export default function ChatBox() {
 	const { setIsChatOpen } = useChat();
+	const [message, setMessage] = useState("");
+
+	const mutation = useMutation({
+		mutationKey: ["chat"],
+		mutationFn: () => {
+			const response = api.post("/chat/send-message", {
+				role: "user",
+				parts: [{ text: message }],
+			});
+			return response;
+		},
+	});
+
+	const handleSendMessage = () => {
+		mutation.mutate();
+	};
+
+	useEffect(() => {
+		if (mutation.data) {
+			console.log(mutation.data);
+		}
+	}, [mutation]);
+
 	return (
 		<div className="w-xs md:w-sm md:h-125 bg-white dark:bg-black text-black dark:text-white fixed bottom-0 right-[3%] rounded-tr-2xl rounded-tl-2xl shadow-lg overflow-hidden border border-zinc-200">
 			<div className="flex flex-col h-full">
@@ -11,7 +37,7 @@ export default function ChatBox() {
 				<div className="w-full h-12 rounded-tr-2xl rounded-tl-2xl shadow-[0_2px_0_0_rgba(0,0,0,0.1)]">
 					<div className="flex px-4 h-full items-center justify-center gap-2">
 						<div className="size-8 bg-black rounded-full">
-							<img src="" alt="" />
+							<img src="#" alt="" />
 						</div>
 
 						<div className="flex-1">Deux Lim</div>
@@ -35,7 +61,7 @@ export default function ChatBox() {
 						<div className="flex items-end gap-2">
 							<div className="w-6 shrink-0 h-full flex items-end">
 								<img
-									src=""
+									src="#"
 									className="size-6 rounded-full bg-black"
 									alt=""
 								/>
@@ -64,11 +90,18 @@ export default function ChatBox() {
 						placeholder="Aa"
 						name=""
 						id=""
+						value={message}
 						className="bg-gray-100 rounded-full px-4 h-8 flex-1 text-sm"
+						onChange={(e) => setMessage(e.target.value)}
 					/>
-					<div className="px-2 cursor-pointer">
+
+					<button
+						type="button"
+						className="px-2 cursor-pointer"
+						onClick={handleSendMessage}
+					>
 						<FaThumbsUp className="text-2xl text-blue-500" />
-					</div>
+					</button>
 				</div>
 			</div>
 		</div>
