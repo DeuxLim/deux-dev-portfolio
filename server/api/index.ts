7697cda4b54a61cb8b24854bc4c71eb5e8a37945
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-28 10:05:34 +0800

    simplify vercel api cors handling

--- server/api/index.ts
+++ server/api/index.ts
@@ -7,13 +7,6 @@ import registerRoutes from "../routes/routes.js";
 const app = express();
 
 app.use(cors(corsOptions));
-app.use((req, res, next) => {
-	if (req.method === "OPTIONS") {
-		return cors(corsOptions)(req, res, next);
-	}
-
-	next();
-});
 app.use(express.json());
 
 registerRoutes(app);
