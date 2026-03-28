016cf56dc2b1489a383b54530266d9f8e6838e14
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-28 10:09:13 +0800

    replace vercel api express bridge

--- server/api/index.ts
+++ server/api/index.ts
@@ -1,14 +1,3 @@
-import express from "express";
-import cors from "cors";
-import serverless from "serverless-http";
-import { corsOptions } from "../config/cors.js";
-import registerRoutes from "../routes/routes.js";
+import { handleApiRequest } from "./_shared.js";
 
-const app = express();
-
-app.use(cors(corsOptions));
-app.use(express.json());
-
-registerRoutes(app);
-
-export default serverless(app);
+export default handleApiRequest;
