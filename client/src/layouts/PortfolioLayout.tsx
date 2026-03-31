307e83abacbb245e3689dede3e40175f618aa374
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-31 16:42:02 +0800

    minor update

--- client/src/layouts/PortfolioLayout.tsx
+++ client/src/layouts/PortfolioLayout.tsx
@@ -2,15 +2,15 @@ import FloatingChat from "@/components/Chatbot/FloatingChat";
 import { Outlet } from "react-router";
 
 export default function PortfolioLayout() {
-	return (
-		<div className="app-shell min-h-dvh bg-cover bg-center">
-			{/* Main Template */}
-			<div className="max-w-4xl mx-auto px-3 sm:px-4 pt-5 sm:pt-6 md:pt-8">
-				<Outlet />
-			</div>
+    return (
+        <div className="app-shell min-h-dvh bg-cover bg-center">
+            {/* Main Template */}
+            <div className="mx-auto max-w-4xl px-3 pt-5 sm:px-4 sm:pt-6 md:pt-8">
+                <Outlet />
+            </div>
 
-			{/* Chat Feature */}
-			<FloatingChat />
-		</div>
-	);
+            {/* Chat Feature */}
+            <FloatingChat />
+        </div>
+    );
 }
