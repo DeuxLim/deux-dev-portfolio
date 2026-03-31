e00756ff104d8cd299a689c02b40c4c6bfb93334
Deux Daniel Lim <limdeux27@gmail.com>
2026-03-31 16:55:14 +0800

    toast routes usage

--- client/src/routes.tsx
+++ client/src/routes.tsx
@@ -3,26 +3,29 @@ import PortfolioLayout from "./layouts/PortfolioLayout";
 import Home from "@/pages/Home";
 import ChatProvider from "./context/Chat/ChatProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import ToastProvider from "./context/Toast/ToastProvider";
 
 const queryClient = new QueryClient();
 
 const routes = createBrowserRouter([
-	{
-		path: "/",
-		element: (
-			<QueryClientProvider client={queryClient}>
-				<ChatProvider>
-					<PortfolioLayout />
-				</ChatProvider>
-			</QueryClientProvider>
-		),
-		children: [
-			{
-				index: true,
-				Component: Home,
-			},
-		],
-	},
+    {
+        path: "/",
+        element: (
+            <QueryClientProvider client={queryClient}>
+                <ToastProvider>
+                    <ChatProvider>
+                        <PortfolioLayout />
+                    </ChatProvider>
+                </ToastProvider>
+            </QueryClientProvider>
+        ),
+        children: [
+            {
+                index: true,
+                Component: Home,
+            },
+        ],
+    },
 ]);
 
 export default routes;
